e15917846bd3ded740cdad80fa47bd334f157ebe
Greg Blumenthal <greg@gmbs-consulting.com>
2024-01-26 12:42:58 -0600

    Unrepeat navbarNav

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,10 @@
 let navbar = `
                 <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="../index.html" data-text="home">home</a></li>
-                <li class="nav-item"><a class="nav-link" href="/pages/Detailed_Tables.html">Detailed Tables</a></li>
-                <li class="nav-item"><a class="nav-link" href="/pages/Table_1.html">Summary Tables</a></li>
-                <li class="nav-item"><a class="nav-link" href="/pages/Pie.html">Summary Pie Charts</a></li>
-                <li class="nav-item"><a class="nav-link" href="/pages/maps_ZIP.html">Maps</a></li>
+                <li class="nav-item"><a id="link-home" class="nav-link" href="../index.html" data-text="home">home</a></li>
+                <li class="nav-item"><a id="link-dt" class="nav-link" href="../pages/Detailed_Tables.html">Detailed Tables</a></li>
+                <li class="nav-item"><a id="link-st" class="nav-link" href="../pages/Table_1.html">Summary Tables</a></li>
+                <li class="nav-item"><a id="link-sp" class="nav-link" href="../pages/Pie.html">Summary Pie Charts</a></li>
+                <li class="nav-item"><a id="link-m" class="nav-link" href="../pages/maps_ZIP.html">Maps</a></li>
             </ul>
     `;
 document.getElementById("navbarNav").innerHTML = navbar;
