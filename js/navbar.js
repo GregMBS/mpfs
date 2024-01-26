3bd8c7acd8e2e7a53a70e3287d73dfa38e39ff97
Greg Blumenthal <greg@gmbs-consulting.com>
2024-01-26 12:22:48 -0600

    fine tune Detail

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+let navbar = `
+                <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="../index.html" data-text="home">home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#" data-text="archives">archives</a></li>
+                <li class="nav-item"><a class="nav-link" href="#" data-text="tags">tags</a></li>
+                <li class="nav-item"><a class="nav-link" href="#" data-text="categories">categories</a></li>
+                <li class="nav-item"><a class="nav-link" href="/pages/Detailed_Tables.html">Detailed Tables</a></li>
+                <li class="nav-item"><a class="nav-link" href="/pages/Table_1.html">Summary Tables</a></li>
+                <li class="nav-item"><a class="nav-link" href="/pages/Pie.html">Summary Pie Charts</a></li>
+                <li class="nav-item"><a class="nav-link" href="/pages/maps_ZIP.html">Maps</a></li>
+            </ul>
+    `;
+document.getElementById("navbarNav").innerHTML = navbar;
