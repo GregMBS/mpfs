2c6953beaf73c5572d191e451dce76a05bab9c25
Greg Blumenthal <greg@gmbs-consulting.com>
2024-02-03 14:44:16 -0600

    All to well-ordered JSON

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,11 @@
 let navbar = `
                 <ul class="navbar-nav">
                 <li class="nav-item"><a id="link-home" class="nav-link" href="../index.html" data-text="home">home</a></li>
-                <li class="nav-item"><a id="link-dt" class="nav-link" href="../pages/Detailed_Tables.html">Detailed Tables</a></li>
-                <li class="nav-item"><a id="link-st" class="nav-link" href="../pages/Table_1.html">Summary Tables</a></li>
+                <li class="nav-item"><a id="link-st" class="nav-link" href="../pages/Summary.html">Summary Tables</a></li>
                 <li class="nav-item"><a id="link-sp" class="nav-link" href="../pages/Pie.html">Summary Pie Charts</a></li>
-                <li class="nav-item"><a id="link-m" class="nav-link" href="../pages/maps_ZIP.html">Maps</a></li>
-                <li class="nav-item"><a id="link-c" class="nav-link" href="../pages/top_10_tables.html">Cities</a></li>
+                <li class="nav-item"><a id="link-dt" class="nav-link" href="../pages/Detailed.html">Detailed Tables</a></li>
+                <li class="nav-item"><a id="link-m" class="nav-link" href="../pages/Maps.html">Maps</a></li>
+                <li class="nav-item"><a id="link-c" class="nav-link" href="../pages/Cities.html">Cities</a></li>
             </ul>
     `;
 document.getElementById("navbarNav").innerHTML = navbar;
