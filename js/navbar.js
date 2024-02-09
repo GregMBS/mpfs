1d47b5f3aa5733888a83a7c6df1610d27e219554
Greg Blumenthal <greg@gmbs-consulting.com>
2024-02-09 10:48:58 -0600

    add discussion to Summary.html, fix pie charts on Pie.html

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 let navbar = `
                 <ul class="navbar-nav">
-                <li class="nav-item"><a id="link-home" class="nav-link" href="../index.html" data-text="home">home</a></li>
+                <li class="nav-item"><a id="link-home" class="nav-link" href="../index.html" data-text="Home">Home</a></li>
                 <li class="nav-item"><a id="link-st" class="nav-link" href="../pages/Summary.html">Summary Tables</a></li>
                 <li class="nav-item"><a id="link-sp" class="nav-link" href="../pages/Pie.html">Summary Pie Charts</a></li>
                 <li class="nav-item"><a id="link-dt" class="nav-link" href="../pages/Detailed.html">Detailed Tables</a></li>
